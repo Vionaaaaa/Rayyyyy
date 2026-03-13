b68a2641115ca7c8955e775b018536a201ca8f0f
Vionaaaaa <124743883+Vionaaaaa@users.noreply.github.com>
2026-03-13 23:42:13 +0800

    Delete docs/.vitepress/configs/nav.ts

--- docs/.vitepress/configs/nav.ts
+++
@@ -1,9 +0,0 @@
-import type { DefaultTheme } from 'vitepress'
-
-export const nav: DefaultTheme.Config['nav'] = [
-  {
-    text: 'Theme',
-    link: '/posts/theme.md',
-    activeMatch: '/posts/theme'
-  },
-]
